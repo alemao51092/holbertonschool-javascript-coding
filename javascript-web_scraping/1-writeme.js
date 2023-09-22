be21b176861e4c3cf77abf5278114d1be1108bfd
alemao51092 <aarielmartinez1111@gmail.com>
2023-09-22 09:09:48 -0700

    task1

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 const fInput = process.argv[3];
-fs.writeFile(process.argv[2], fInput, (err) => {
+fs.writeFile(process.argv[2], 'utf8', fInput, (err) => {
   if (err) throw err;
 });
