00f1999cb49b9849b3652faef30a101edfd0f2b5
alemao51092 <aarielmartinez1111@gmail.com>
2023-09-22 09:07:39 -0700

    task0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const fs = require('fs')
-const rfile = process.argv[2]
-fs.readFile(rfile, "utf8", (err, inputD) => {
-    if (err) throw err;
-    console.log(inputD);
-})
+const fs = require('fs');
+const rfile = process.argv[2];
+fs.readFile(rfile, 'utf8', (err, inputD) => {
+  if (err) throw err;
+  console.log(inputD);
+});
--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-const fInput = process.argv[3]
+const fInput = process.argv[3];
 fs.writeFile(process.argv[2], fInput, (err) => {
-    if (err) throw err;
-    })
+  if (err) throw err;
+});
